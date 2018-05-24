70de34d16a27678aea43e9adce8796e3e9d41b67
Kevin Tran <kevin.tran@leonardo.com>
2018-05-24 14:21:46 -0400

    Fixing some errors from original blog

---
+++ spring-batch-partition-master/README.md
@@ -0,0 +1,5 @@
+The sample code for Batch Partition which I copied and edited from https://github.com/walkingtechie/spring-batch-partition
+
+Some updated code:
+- Replace MySQL by an Embedded DB.
+- Fix error when missing logger or setter/getter fields.
---
+++ spring-batch-partition-master/src/main/java/com/walking/techie/tasklet/DummyTasklet.java
@@ -0,0 +1,20 @@
+package com.walking.techie.tasklet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+@Slf4j
+public class DummyTasklet implements Tasklet {
+  public static final Logger logger = LoggerFactory.getLogger(DummyTasklet.class);
+  @Override
+  public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+      throws Exception {
+    logger.info("Dummy Tasklet called.");
+    return RepeatStatus.FINISHED;
+  }
+}

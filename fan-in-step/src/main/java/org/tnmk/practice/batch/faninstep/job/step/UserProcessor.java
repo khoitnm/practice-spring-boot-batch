720b020727df2deb1e1d25bee3c808582836ffa4
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2018-06-03 16:20:45 -0400

    Clone 'chunk-model-with-concurrent-jobs' to 'fan-in-step' module.

---
+++ fan-in-step/src/main/java/org/tnmk/practice/batch/faninstep/job/step/UserProcessor.java
@@ -0,0 +1,18 @@
+package org.tnmk.practice.batch.faninstep.job.step;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.tnmk.common.batch.joblauncher.JobLauncherHelper;
+import org.tnmk.practice.batch.faninstep.model.User;
+
+public class UserProcessor implements ItemProcessor<User, User> {
+
+    @Value("#{jobParameters['" + JobLauncherHelper.PARAM_JOB_INSTANCE_ID + "']}")
+    private String jobInstanceId;
+
+    @Override
+    public User process(User item) {
+        System.out.println("JobInstanceId: " + jobInstanceId + ",\tThread: " + Thread.currentThread().getName() + "\t{" + item.getId() + ", " + item.getUsername() + "}");
+        return item;
+    }
+}
---
+++ fan-in-step/src/test/java/org/tnmk/practice/batch/faninstep/RunJobsInSequenceApplicationTests.java
@@ -0,0 +1,25 @@
+package org.tnmk.practice.batch.faninstep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.tnmk.practice.batch.faninstep.joblauncher.FileProcessingJobLauncherHelper;
+
+import java.util.concurrent.ExecutionException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunJobsInSequenceApplicationTests {
+
+    @Autowired
+    FileProcessingJobLauncherHelper fileProcessingJobLauncherHelper;
+
+    @Test
+    public void startFileProcessingBatchJob() throws InterruptedException, ExecutionException {
+        fileProcessingJobLauncherHelper.startFileProcessJob("/users_7K.csv", "out/csv_7K/users.processed.");
+        fileProcessingJobLauncherHelper.startFileProcessJob("/heroes.csv", "out/csv/heroes.processed.");
+    }
+
+}
